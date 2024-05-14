13a5ecd948cd1bbd753bb04af42b6d91ce07560d
soyshan <shanbu131@gmail.com>
2024-05-14 17:22:56 +0200

    fix:changes in the app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import './database/db.js';
 import express from 'express';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import partnerRoutes from './routes/partnerRoutes.js';
-import loginRoutes from './routes/loginRoutes.js';
+import userRoutes from './routes/UserRoutes.js';
+import partnerRoutes from './routes/PartnerRoutes.js';
+import loginRoutes from './routes/LoginRoutes.js';
 import activitiesRouter from './routes/ActivitiesRoutes.js';
 import categoryRoutes from './routes/CategoryRoutes.js';
 import centerRoutes from './routes/CenterRoutes.js'
