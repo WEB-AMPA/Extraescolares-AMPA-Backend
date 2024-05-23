56b152098a93bddd5f09f0559f8cb36fdc6dcb4a
soyshan <shanbu131@gmail.com>
2024-05-23 10:31:09 +0200

    fix: restore some routes that missed because of the merge

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ import scheduleDaysRouter from "./routes/ScheduleDaysRoutes.js";
 import scheduleHoursRouter from "./routes/ScheduleHoursRoutes.js";
 import activitiesStudentsRouter from "./routes/ActivitiesStudentsRoutes.js";
 import attendanceRoutes from './routes/attendanceRoutes.js'
-
+import breakfastRoutes from './routes/breakfastRoutes.js'
 import mongoose from "mongoose";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -38,8 +38,8 @@ app.use("/api/activities", activitiesRouter);
 app.use("/api/scheduleDays", scheduleDaysRouter);
 app.use("/api/scheduleHours", scheduleHoursRouter);
 app.use("/api/activitiesStudents", activitiesStudentsRouter);
-
-app.use('/', attendanceRoutes)
+app.use("/", breakfastRoutes);
+app.use("/", attendanceRoutes);
 
 // Puerto en el que se ejecutará el servidor
 const PORT = process.env.PORT ?? 3000;
