32a288e64b272fe07eaf426b5006f70f0a2bb691
soyshan <shanbu131@gmail.com>
2024-06-07 16:52:13 +0200

    fix: student route because duplicated route

--- routes/StudentsRoutes.js
+++ routes/StudentsRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/', studentsController.createStudent);
 router.get('/', studentsController.getAllStudents);
-router.get('/withbreakfast', studentsController.getStudentsWithBreakfast);
+
 router.get('/:id', studentsController.getStudentById);
 router.put('/:id', studentsController.updateStudent);
 router.delete('/:id', studentsController.deleteStudent);
