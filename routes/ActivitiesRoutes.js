5b93bd2ff5df3a3f850a74fe478566cf5ec5ab56
soyshan <shanbu131@gmail.com>
2024-06-12 22:42:07 +0200

    fix: comment routes of users and activities to test it in the front

--- routes/ActivitiesRoutes.js
+++ routes/ActivitiesRoutes.js
@@ -4,22 +4,22 @@ import { authenticate, authorize } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', authenticate, authorize(['admin']), activitiesController.createActivity);
-// router.get('/', authenticate, authorize(['admin']), activitiesController.getAllActivities);
-// router.get('/:id', authenticate, authorize(['admin']), activitiesController.getActivityById);
-router.put('/:id', authenticate, authorize(['admin']), activitiesController.updateActivity);
-router.delete('/:id', authenticate, authorize(['admin']), activitiesController.deleteActivity);
-router.post('/assign', authenticate, authorize(['admin']), activitiesController.assignActivityToStudent);
-
-
-
-
-
-router.get('/',  activitiesController.getAllActivities);
-router.get('/:id',  activitiesController.getActivityById);
+// router.post('/', authenticate, authorize(['admin']), activitiesController.createActivity);
+// // router.get('/', authenticate, authorize(['admin']), activitiesController.getAllActivities);
+// // router.get('/:id', authenticate, authorize(['admin']), activitiesController.getActivityById);
 // router.put('/:id', authenticate, authorize(['admin']), activitiesController.updateActivity);
 // router.delete('/:id', authenticate, authorize(['admin']), activitiesController.deleteActivity);
 // router.post('/assign', authenticate, authorize(['admin']), activitiesController.assignActivityToStudent);
+
+
+
+
+router.post('/', activitiesController.createActivity);
+router.get('/',  activitiesController.getAllActivities);
+router.get('/:id',  activitiesController.getActivityById);
+router.put('/:id', activitiesController.updateActivity);
+router.delete('/:id',  activitiesController.deleteActivity);
+router.post('/assign', activitiesController.assignActivityToStudent);
 
 
 export default router;
