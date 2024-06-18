c68461d3800bda2610a02df6a37897808f5dd1a1
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-06-18 13:11:23 +0200

    feat: update routes

--- routes/ActivitiesStudentsRoutes.js
+++ routes/ActivitiesStudentsRoutes.js
@@ -4,9 +4,9 @@ import { authenticate, authorize } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', authenticate, authorize(['admin']), activitiesStudentsController.getAllAssignments);
+router.get('/', authenticate, authorize(['admin', 'partner']), activitiesStudentsController.getAllAssignments);
 router.get('/by-activity/:activityId', authenticate, authorize(['admin']), activitiesStudentsController.getStudentsByActivity);
-router.get('/:id', authenticate, authorize(['admin']), activitiesStudentsController.getAssignmentById);
+router.get('/:id', authenticate, authorize(['admin','partner']), activitiesStudentsController.getAssignmentById);
 router.post('/', authenticate, authorize(['admin']), activitiesStudentsController.createAssignment);
 router.put('/:id', authenticate, authorize(['admin']), activitiesStudentsController.updateAssignment);
 router.delete('/:id', authenticate, authorize(['admin']), activitiesStudentsController.deleteAssignment);
